8caaf3049c01ff5458b7cd23fa205389f08a600b
maryandn <maryandnmail@gmail.com>
2025-11-21 19:53:37 +0200

    fix

--- sites/tv.blue.ch/tv.blue.ch.config.js
+++ sites/tv.blue.ch/tv.blue.ch.config.js
@@ -6,7 +6,7 @@ dayjs.extend(utc)
 
 module.exports = {
   site: 'tv.blue.ch',
-  days: 2,
+  days: 3,
   url: function ({ channel, date }) {
     return `https://services.sg101.prd.sctv.ch/catalog/tv/channels/list/(ids=${
       channel.site_id
